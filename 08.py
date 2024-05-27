a27eb9eaa990170d5d921a8ba64844a54f6ab437
ludamalik <ludamalik@ukr.net>
2024-05-27 22:42:10 +0300

    08.py

---
+++ 08.py
@@ -0,0 +1,7 @@
+#Завдання 8
+
+Days=int(input("Дні: "))
+Hours=int(input("Години: "))
+Minutes=int(input("Хвилини: "))
+Seconds=int(input("Секунди: "))
+print("The amaunts of seconds:",(Days*24*60*60)+(Hours*60*60)+(Minutes*60)+Seconds)
