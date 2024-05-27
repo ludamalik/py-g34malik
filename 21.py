db0701ee8e54bbf30721b36ff6e058ec00387861
ludamalik <ludamalik@ukr.net>
2024-05-27 22:49:05 +0300

    21.py

---
+++ 21.py
@@ -0,0 +1,11 @@
+#Завдання 21
+
+n=int(input("Кількість хвилин 1: "))
+hours=n//60
+print(hours,n-hours*60,sep=" ")
+n=int(input("Кількість хвилин 2: "))
+hours=n//60
+print(hours,n-hours*60,sep=" ")
+n=int(input("Кількість хвилин 3: "))
+hours=n//60
+print(hours,n-hours*60,sep=" ")
