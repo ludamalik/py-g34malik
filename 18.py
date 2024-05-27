96ea27cbd0abdbcc805885eefadc264a6108f787
ludamalik <ludamalik@ukr.net>
2024-05-27 22:47:31 +0300

    18.py

---
+++ 18.py
@@ -0,0 +1,11 @@
+#Завдання 18
+
+a1=int(input("Введіть а1: "))
+a2=int(input("Введіть а2: "))
+a3=int(input("Введіть а3: "))
+km1=a1//1000
+km2=a2//1000
+km3=a3//1000
+print(km1)
+print(km2)
+print(km3)
