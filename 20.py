bfa9016a41dbd2d7c3321287bdf87b341349f943
ludamalik <ludamalik@ukr.net>
2024-05-27 22:48:26 +0300

    20.py

---
+++ 20.py
@@ -0,0 +1,26 @@
+#Завдання 20
+
+second=(int(input("Значення в секундах1: ")))
+day=second//86400
+second=second%86400
+hour=second//3600
+second=second%3600
+minute=second//60
+second=second%60
+print(str(day) + " day(s), " + str(hour) + "hour(s), " + str(minute) + " minute(s), " + str(second) + " second(s).")
+second=(int(input("Значення в секундах2: ")))
+day=second//86400
+second=second%86400
+hour=second//3600
+second=second%3600
+minute=second//60
+second=second%60
+print(str(day) + " day(s), " + str(hour) + "hour(s), " + str(minute) + " minute(s), " + str(second) + " second(s).")
+second=(int(input("Значення в секундах3: ")))
+day=second//86400
+second=second%86400
+hour=second//3600
+second=second%3600
+minute=second//60
+second=second%60
+print(str(day) + " day(s), " + str(hour) + "hour(s), " + str(minute) + " minute(s), " + str(second) + " second(s).")
