729f5bb2b0228d6d0400c1536b3b63c7d22cb788
ludamalik <ludamalik@ukr.net>
2024-06-14 21:37:32 +0300

    main.py

---
+++ lecture6/main.py
@@ -0,0 +1,33 @@
+from .menu import menu, calc_help
+from .utils import extract, result
+
+
+def main():
+    while True:
+        choice = menu()
+
+        if choice == 'q':
+            print('Thank you for using Super Calc!')
+            break
+
+        if choice == 'h':
+            calc_help()
+            continue
+
+        if choice == 'c':
+            entry = input("Enter x operator y (e.g., 2 + 2): ")
+            a, b, operator = extract(entry)
+
+            if not operator:
+                calc_help("Invalid format or unsupported operator.")
+                continue
+
+            res, err = result(a, b, operator)
+            if err:
+                print(err)
+            else:
+                print(f"{a} {operator} {b} = {res}")
+
+
+if __name__ == "__main__":
+    main()
